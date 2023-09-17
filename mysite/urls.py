cee1c49ad2fd1a46ac7af0e2d3fcb184f9dfa163
waratpoovorakit <warat.po@ku.th>
2023-09-17 21:40:19 +0700

    update information and fix some bugs

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,21 +1,23 @@
-from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.contrib.messages import constants as messages_constants
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/polls/'), name='index'),
     path('polls/', include('polls.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('polls/', include('polls.urls'), name='index'),
     path('admin/', admin.site.urls),
 ]
-from django.contrib.messages import constants as messages_constants
+
+
 MESSAGE_TAGS = {
     messages_constants.ERROR: 'danger',
 }
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
