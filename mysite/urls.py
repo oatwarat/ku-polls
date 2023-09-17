d1e25dea9afb8090191852f16d00d4b404840350
waratpoovorakit <warat.po@ku.th>
2023-09-17 19:22:30 +0700

    update settings and urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/polls/'), name='index'),
     path('polls/', include('polls.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('polls/', include('polls.urls'), name='index'),
     path('admin/', admin.site.urls),
 ]
 from django.contrib.messages import constants as messages_constants
