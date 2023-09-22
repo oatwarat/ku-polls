582aedb6e1fca81c6346425352b485952dd061b0
waratpoovorakit <warat.po@ku.th>
2023-09-23 03:45:42 +0700

    improve features and styling of website

--- polls/tests.py
+++ polls/tests.py
@@ -169,7 +169,7 @@ class QuestionDetailViewTests(TestCase):
         future_question = create_question(question_text='Future question', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 302)  # Update this line to expect a 302 redirect
+        self.assertEqual(response.status_code, 302)
 
     def test_past_question(self):
         """
