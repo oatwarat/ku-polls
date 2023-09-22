6319e657acd2dd3d075594533aec76ce2f330d36
waratpoovorakit <warat.po@ku.th>
2023-09-23 03:47:40 +0700

    update some website features

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 from django.contrib.messages import constants as messages_constants
 from django.views.generic import RedirectView
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/polls/'), name='index'),
     path('polls/', include('polls.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
-
 
 MESSAGE_TAGS = {
     messages_constants.ERROR: 'danger',
---
+++ mysite/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
